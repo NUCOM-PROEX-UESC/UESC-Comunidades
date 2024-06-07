59fab495533517ee75ba75d119f5ebda974e5b71
Bruno Felipe <brunofelipecoder@gmail.com>
2024-06-07 13:57:48 -0300

    FIX: Números PROEX com carrossel

--- Js/numeros_proex.js
+++ Js/numeros_proex.js
@@ -1,12 +1,13 @@
 const data = [
-    { number: "+6 Milhões", description: "beneficiados em atividades de extensão" },
-    { number: "+320", description: "projetos de extensão distribuídos entre os 11 departamentos" },
-    { number: "+10", description: "projetos de extensão premiados em 2022" },
-    { number: "+30", description: "municípios atendidos com atividades de extensão" },
-    { number: "+110", description: "atividades de extensão em assentamentos" },
-    { number: "+490", description: "bolsas de extensão distribuídas entre os estudantes de graduação" },
-    { number: "+230", description: "bolsistas voluntários em atividades de extensão" },
-    { number: "+850 Mil", description: "verba destinada para atividades de extensão em 2022" },
+    { number: "20%", description: "de aumento no número de atividades de extensão em 2023" },
+    { number: "329", description: "atividades de extensão realizadas em 2023" },
+    { number: "283.390", description: "pessoas atendidas pelas atividades de extensão em 2023" },
+    { number: "652", description: "discentes envolvidos nas atividades de extensão em 2023" },
+    { number: "296", description: "bolsas e beneficiando 452 alunos ao longo do ano de 2023" },
+    { number: "210", description: "discentes foram beneficiados como voluntários" },
+    { number: "44.527", description: "pessoas atendidas na área da Cultura em 2023" },
+    { number: "35.884", description: "pessoas atendidas na área da Saúde em 2023" },
+    { number: "+270%", description: "Crescimento do alcance no Instagram de 2022 para 2023" },
 ];
 
 let currentIndex = 0;
@@ -16,7 +17,6 @@ function updateNumbers() {
         { number: document.getElementById('number1'), description: document.getElementById('description1') },
         { number: document.getElementById('number2'), description: document.getElementById('description2') },
         { number: document.getElementById('number3'), description: document.getElementById('description3') },
-        { number: document.getElementById('number4'), description: document.getElementById('description4') },
     ];
 
     for (let i = 0; i < elements.length; i++) {
@@ -29,7 +29,7 @@ function updateNumbers() {
 function updateIndicators() {
     const indicators = document.querySelectorAll('.indicator');
     indicators.forEach((indicator, index) => {
-        if (index === Math.floor(currentIndex / 4)) {
+        if (index === Math.floor(currentIndex / 3)) {
             indicator.classList.add('active');
         } else {
             indicator.classList.remove('active');
@@ -38,14 +38,14 @@ function updateIndicators() {
 }
 
 function nextSet() {
-    currentIndex = (currentIndex + 4) % data.length;
+    currentIndex = (currentIndex + 3) % data.length;
     updateNumbers();
     updateIndicators();
 }
 
 document.querySelectorAll('.indicator').forEach((indicator, index) => {
     indicator.addEventListener('click', () => {
-        currentIndex = index * 4;
+        currentIndex = index * 3;
         updateNumbers();
         updateIndicators();
     });
