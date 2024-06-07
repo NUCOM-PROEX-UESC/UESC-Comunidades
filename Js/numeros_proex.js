e3ec5aa0445985f083170d7fa6dca6d4a80c25b3
BrunoFelipeDevt <brunofelipecoder@gmail.com>
2024-06-07 18:21:06 -0300

    FIX: Adicionando setinhas, falta corrigir a versão mobile

--- Js/numeros_proex.js
+++ Js/numeros_proex.js
@@ -1,4 +1,5 @@
 let currentIndex = 0;
+let autoSlideInterval;
 
 function updateCarousel() {
     const container = document.querySelector('.numbers-container');
@@ -22,14 +23,40 @@ function nextSet() {
     updateIndicators();
 }
 
+function prevSet() {
+    currentIndex = (currentIndex - 3 + 9) % 9;
+    updateCarousel();
+    updateIndicators();
+}
+
+document.querySelector('.next').addEventListener('click', () => {
+    nextSet();
+    resetAutoSlide();
+});
+
+document.querySelector('.prev').addEventListener('click', () => {
+    prevSet();
+    resetAutoSlide();
+});
+
 document.querySelectorAll('.indicator').forEach((indicator, index) => {
     indicator.addEventListener('click', () => {
         currentIndex = index * 3;
         updateCarousel();
         updateIndicators();
+        resetAutoSlide();
     });
 });
 
-setInterval(nextSet, 10000);
+function startAutoSlide() {
+    autoSlideInterval = setInterval(nextSet, 10000);
+}
+
+function resetAutoSlide() {
+    clearInterval(autoSlideInterval);
+    startAutoSlide();
+}
+
+startAutoSlide();
 updateCarousel();
 updateIndicators();  // Initialize with first set of data
